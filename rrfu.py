b4f33c633ad7633f49a54fe334885186336bfee8
yisi-xj <1751250104@qq.com>
2025-06-07 23:19:53 +0800

    Update rrfu.py

--- rrfu.py
+++ rrfu.py
@@ -2,18 +2,6 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
-def xavier_init(module, gain=1.0, bias=0.0, distribution='normal'):
-    assert distribution in ['uniform', 'normal']
-    if hasattr(module, 'weight') and module.weight is not None:
-        if distribution == 'uniform':
-            nn.init.xavier_uniform_(module.weight, gain=gain)
-        else:
-            nn.init.xavier_normal_(module.weight, gain=gain)
-    if hasattr(module, 'bias') and module.bias is not None:
-        nn.init.constant_(module.bias, bias)
-
-
 def normal_init(module, mean=0.0, std=1.0, bias=0.0):
     if hasattr(module, 'weight') and module.weight is not None:
         nn.init.normal_(module.weight, mean, std)
